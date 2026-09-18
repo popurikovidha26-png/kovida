6d658aa385160fd1659707318ba8985f2c7020ab
popurikovidha26-png <popurikovidha26@gmail.com>
2026-09-18 14:54:23 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,6 @@
+length=float(input("Enter the length: "))
+breadth=float(input("Enter the breadth: "))
+area=length*breadth
+perimeter=2*(length+breadth)
+print("Area of the rectangle is:", area)
+print("Perimeter of the rectangle is:", perimeter)
